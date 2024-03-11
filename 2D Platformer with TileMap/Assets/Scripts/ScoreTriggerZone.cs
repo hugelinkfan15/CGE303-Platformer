deba510df8b9612754a942e48028b935844468b2
hugelinkfan15 <158606627+hugelinkfan15@users.noreply.github.com>
2024-03-10 22:54:00 -0500

    Finished project

--- 2D Platformer with TileMap/Assets/Scripts/ScoreTriggerZone.cs
+++ 2D Platformer with TileMap/Assets/Scripts/ScoreTriggerZone.cs
@@ -5,12 +5,16 @@ using UnityEngine;
 public class ScoreTriggerZone : MonoBehaviour
 {
     bool active = true;
+
+    [SerializeField] private AudioClip thisTriggered;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (active && collision.gameObject.tag == "Player")
         {
             active = false;
             ScoreManager.score++;
+            SoundFXManager.instance.PlaySoundFXCLip(thisTriggered, transform, 1f);
+            gameObject.SetActive(false);
         }
     }
 }
