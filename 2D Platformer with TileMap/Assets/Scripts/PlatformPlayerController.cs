302ceab65b7024fe5effe6917b95e2650f130557
hugelinkfan15 <158606627+hugelinkfan15@users.noreply.github.com>
2024-03-31 22:12:15 -0500

    Animations done

--- 2D Platformer with TileMap/Assets/Scripts/PlatformPlayerController.cs
+++ 2D Platformer with TileMap/Assets/Scripts/PlatformPlayerController.cs
@@ -15,12 +15,15 @@ public class PlatformPlayerController : MonoBehaviour
     private bool isGrounded;
     private float horizontalInput;
 
+    private Animator animator;
+
     [SerializeField] private AudioClip jumpSoundClip;
     // Start is called before the first frame update
     void Start()
     {
         //Get the Rigidbody2D component attached to the GameObject
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
 
         //Ensure the groundCheck variable is assigned
         if ( groundCheck == null)
@@ -52,8 +55,17 @@ public class PlatformPlayerController : MonoBehaviour
         //Move the player using Rigidbody2D in FixedUpdate
         rb.velocity = new Vector2(horizontalInput * moveSpeed, rb.velocity.y);
 
+        //set animator parameter xVelocityAbs to absolute value of x velocity
+        animator.SetFloat("xVelocityAbs", Mathf.Abs(rb.velocity.x));
+
+        //set animator parameter yVelocity to y velocity
+        animator.SetFloat("yVelocity",rb.velocity.y);
+
         //Check if player is grounded
         isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+
+        //set animator parameter onGround to isGrounded
+        animator.SetBool("onGround", isGrounded);
 
         //Optionally, you can add animations or other behavior here based on player state
         if(horizontalInput > 0)
