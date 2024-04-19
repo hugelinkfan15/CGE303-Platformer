09ad369734ec72a4a25b6d30988961c91d3b813f
hugelinkfan15 <158606627+hugelinkfan15@users.noreply.github.com>
2024-04-19 12:25:12 -0500

    health bar added

--- 2D Platformer with TileMap/Assets/Scripts/Enemy.cs
+++ 2D Platformer with TileMap/Assets/Scripts/Enemy.cs
@@ -8,9 +8,28 @@ public class Enemy : MonoBehaviour
 
     public GameObject deathEffect;
 
+    private DisplayBar healthBar;
+
+    public void Start()
+    {
+        healthBar = GetComponentInChildren<DisplayBar>();
+
+        if (healthBar == null)
+        {
+            //If the health bar is not found, log an error
+            Debug.LogError("HealthBar (DisplayBar script) not found");
+        }
+
+        healthBar.SetMaxValue(health);
+
+    }
+
     public void TakeDamage(int damage)
     {
         health -= damage;
+
+        healthBar.SetValue(health);
+
 
         if(health<=0)
         {
